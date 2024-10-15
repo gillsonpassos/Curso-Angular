3b388915aacb0c3b1edae98c6dabde87a9f3ae6d
Gilson <oliveirapassos07@outlook.com>
2024-10-15 08:32:37 -0300

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls:['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Atributos-Propreidades';
+  inputText = "Texto Inicial Alterado";
+  inputType = "text";
+  isDisabled = true;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  setPasswordTypeInput() {
+    this.inputType = "password";
+  }
+
+  setTextTypeInput() {
+    this.inputType = "text";
+  }
+
+  logInputText() {
+    console.log(this.inputText);
+  }
 }
+
