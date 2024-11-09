8213a5196e7cac02fa82c54348ccef02adc23f37
Gilson <oliveirapassos07@outlook.com>
2024-11-08 21:15:14 -0300

    como funciona o NgIf

--- Diretiva-Estrutural-Nglf/src/app/app.component.ts
+++ Diretiva-Estrutural-Nglf/src/app/app.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Diretiva-Estrutural-Nglf';
+  minhaProp = true;
+  pessoa: any = {
+    status: 1,
+    name: undefined,
+  };
+
+  toggleProp() {
+    this.minhaProp = !this.minhaProp;
+  }
+
+  isTruthy() {
+    // return {};
+    // return [];
+    // return true;
+    // return 1;
+    // return "teste";
+    // return 'teste';
+    // return "0";
+    // return "false";
+    // return this.pessoa.status;
+  }
+
+  isFalsy() {
+    // return false;
+    // return 0;
+    // return "";
+    // return '';
+    // return null;
+    // return undefined;
+    // return -0;
+    // return NaN;
+    // return this.pessoa.name;
+    return this.pessoa.endereço?.rua;
+  }
 }
