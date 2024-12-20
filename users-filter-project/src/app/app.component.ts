837314f3eda921f0b166e6889755fb7209386187
Gilson <oliveirapassos07@outlook.com>
2024-12-20 13:04:18 -0300

    Passagem de  dados para o componente de detalhes

--- users-filter-project/src/app/app.component.ts
+++ users-filter-project/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { IUser } from './interfaces/user/user.interface';
+import { UserList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  userSelected: IUser = UserList[1];
+}
